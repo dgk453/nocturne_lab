2c492fed3b0b51f57084ddfd5d748954c9a123dc
daphnecor <cor.daphne@gmail.com>
2024-01-26 09:02:36 -0500

    Fixes and cleanup

--- experiments/hr_rl/run_hr_ppo_cli.py
+++ experiments/hr_rl/run_hr_ppo_cli.py
@@ -4,13 +4,12 @@ import os
 from datetime import datetime
 from random import randint
 from time import sleep
-
+from typing import Callable
 import numpy as np
 import torch
 import typer
 from box import Box
 from stable_baselines3.common.policies import ActorCriticPolicy
-
 import wandb
 
 # Permutation equivariant network
@@ -29,7 +28,7 @@ from utils.sb3.callbacks import CustomMultiAgentCallback
 from utils.sb3.reg_ppo import RegularizedPPO
 from utils.string_utils import datetime_to_str
 
-os.environ["WANDB__SERVICE_WAIT"] = "200"
+os.environ["WANDB__SERVICE_WAIT"] = "300"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -45,37 +44,64 @@ LAYERS_DICT = {
     "large": [256, 128, 64],
 }
 
+TIME_STEPS_DICT = {
+    'A': [10, 20, 40, 60],
+    'B': [10, 20, 30, 40, 50, 60, 70],
+}
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
+
 
 def run_hr_ppo(
     sweep_name: str = exp_config.group,
-    steer_disc: int = 5,
-    accel_disc: int = 5,
     ent_coef: float = 0.001,
     vf_coef: float = 0.5,
-    seed: int = 42,
     mini_batch_size: int = 256,
+    seed: int = 42,
     lr: float = 3e-4,
     arch_road_objects: str = "large",
     arch_road_graph: str = "medium",
     arch_shared_net: str = "small",
     activation_fn: str = "tanh",
-    position_target_tolerance: float = 2.0,
     dropout: float = 0.0,
-    total_timesteps: int = 5_000_000,
+    randomize_goals: int = 1,
+    rand_goals_timesteps: str = 'A',
+    total_timesteps: int = 10_000_000,
     num_files: int = 1000,
     reg_weight: float = 0.0,
-    num_controlled_veh: int = 20,
+    num_controlled_veh: int = 100,
+    pretrained_model: str = "None",
 ) -> None:
     """Train RL agent using PPO with CLI arguments."""
+    
     # ==== Overwrite default settings ==== #
     # Environment
-    exp_config.ppo.learning_rate = lr
-    exp_config.ppo.batch_size = mini_batch_size
-    exp_config.seed = seed
-    env_config.steer_disc = steer_disc
-    env_config.accel_disc = accel_disc
-    env_config.num_files = num_files
-    env_config.rew_cfg.position_target_tolerance = position_target_tolerance
+    env_config.num_files = num_files   
+    
+    # Randomize goals
+    if randomize_goals == 1:
+        env_config.target_positions.randomize_goals = True
+        env_config.target_positions.time_steps_list = TIME_STEPS_DICT[rand_goals_timesteps]
+    elif randomize_goals == 0:
+        env_config.target_positions.randomize_goals = False
 
     # Set the number of vehicles to control per scene
     # If set to 1 we're doing single-agent RL (during training)
@@ -85,10 +111,13 @@ def run_hr_ppo(
 
     # Experiment
     exp_config.seed = seed
-    exp_config.ent_coef = ent_coef
-    exp_config.vf_coef = vf_coef
+    exp_config.ppo.vf_coef = vf_coef
+    exp_config.ppo.ent_coef = ent_coef
+    exp_config.ppo.learning_rate = lr
+    exp_config.ppo.batch_size = mini_batch_size
     exp_config.learn.total_timesteps = total_timesteps
     exp_config.reg_weight = reg_weight
+    
     # Model architecture
     exp_config.model_config.arch_ro = arch_road_objects
     exp_config.model_config.arch_rg = arch_road_graph
@@ -105,8 +134,8 @@ def run_hr_ppo(
             "dropout": dropout,
         }
     )
+    
     # ==== Overwrite default settings ==== #
-
     # Ensure reproducability
     init_seed(env_config, exp_config, exp_config.seed)
 
@@ -115,45 +144,35 @@ def run_hr_ppo(
         config=env_config,
         num_envs=env_config.max_num_vehicles,
     )
-
-    # Set up run_id
-    # Sleep for a random number of seconds so two runs never have the same run_id
-    sleep(randint(2, 15))
-    datetime_ = datetime_to_str(dt=datetime.now())
-    run_id = f"{datetime_}" if exp_config.track_wandb else None
-
+    
     # Add scene to config
     exp_config.scene = env.filename
 
     if exp_config.track_wandb:
         run = wandb.init(
             project=exp_config.project,
-            name=run_id,
             group=sweep_name,
             config={**exp_config, **env_config, **model_config},
-            id=run_id,
             **exp_config.wandb,
         )
 
     # Set device
     exp_config.ppo.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # Logging
     logging.info(f"Created env. Max # agents = {env_config.max_num_vehicles}.")
-    logging.info(f"Learning in {len(env.env.files)} scene(s) using {exp_config.ppo.device}")
+    logging.info(f"Learning in {len(env.env.files)} scene(s). Device = {exp_config.ppo.device}.")
 
     logging.info(f"--- obs_space: {env.observation_space.shape[0]} ---")
-    logging.info(f"Action_space\n: {env.env.idx_to_actions}")
-    logging.info(
-        f"Pos target tol: {env_config.rew_cfg.position_target_tolerance} | "
-        f"Speed target: {env_config.rew_cfg.speed_target} - tol: {env_config.rew_cfg.speed_target_tolerance}"
-    )
+    logging.info(f"--- act_space: {env.action_space.n} ---")
+    logging.info(f"--- randomized goals: {env_config.target_positions.randomize_goals} ---")
 
     # Initialize custom callback
     custom_callback = CustomMultiAgentCallback(
         env_config=env_config,
         exp_config=exp_config,
         video_config=video_config,
-        wandb_run=run if run_id is not None else None,
+        wandb_run=run,
     )
 
     # Make scene init video to check expert actions
@@ -176,40 +195,44 @@ def run_hr_ppo(
         human_policy.load_state_dict(saved_variables["state_dict"])
         human_policy.to(exp_config.ppo.device)
 
+    # Proceed training from a pretrained model
+    if pretrained_model != 'None':
+        model = RegularizedPPO.load(pretrained_model)
+        model.set_env(env)
+        logging.info(f"Loaded pretrained model: {pretrained_model} --- continue training")
+        
     # Set up PPO
-    model = RegularizedPPO(
-        learning_rate=lr,
-        reg_policy=human_policy,
-        reg_weight=exp_config.reg_weight,  # Regularization weight; lambda
-        env=env,
-        n_steps=exp_config.ppo.n_steps,
-        policy=LateFusionPolicy,
-        batch_size=exp_config.ppo.batch_size,
-        ent_coef=exp_config.ppo.ent_coef,
-        vf_coef=exp_config.ppo.vf_coef,
-        seed=exp_config.seed,  # Seed for the pseudo random generators
-        verbose=exp_config.verbose,
-        tensorboard_log=f"runs/{run_id}" if run_id is not None else None,
-        device=exp_config.ppo.device,
-        env_config=env_config,
-        mlp_class=LateFusionNet,
-        mlp_config=model_config,
-    )
+    else:  
+        model = RegularizedPPO(
+            learning_rate=linear_schedule(exp_config.ppo.learning_rate),
+            reg_policy=human_policy,
+            reg_weight=exp_config.reg_weight,  # Regularization weight; lambda
+            env=env,
+            n_steps=exp_config.ppo.n_steps,
+            policy=LateFusionPolicy,
+            batch_size=exp_config.ppo.batch_size,
+            ent_coef=exp_config.ppo.ent_coef,
+            vf_coef=exp_config.ppo.vf_coef,
+            seed=exp_config.seed,  # Seed for the pseudo random generators
+            verbose=exp_config.verbose,
+            tensorboard_log=f"runs/{run.id}" if run is not None else None,
+            device=exp_config.ppo.device,
+            env_config=env_config,
+            mlp_class=LateFusionNet,
+            mlp_config=model_config,
+        )        
+        
     # Log number of trainable parameters
     policy_params = filter(lambda p: p.requires_grad, model.policy.parameters())
     params = sum(np.prod(p.size()) for p in policy_params)
     exp_config.n_policy_params = params
     logging.info(f"Policy | trainable params: {params:,} \n")
 
-    # Architecture
-    # logging.info(f"Policy | arch: \n {model.policy}")
-
     # Learn
     model.learn(
         **exp_config.learn,
         callback=custom_callback,
     )
-
     if exp_config.track_wandb:
         run.finish()
 
